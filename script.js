3003dca745ca445c3a97cddf6b991f12be4fa5e4
yus Projects <99153148+yus2000@users.noreply.github.com>
2026-08-24 22:50:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -319,10 +319,10 @@ function startDigitalClock() {
     const now = new Date();
     const hours = String(now.getHours()).padStart(2, '0');
     const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
+    // const seconds = String(now.getSeconds()).padStart(2, '0');
     
     // Format: HH:MM:SS
-    clockEl.textContent = `${hours}:${minutes}:${seconds}`;
+    clockEl.textContent = `${hours}:${minutes}`;
   }
 
   updateClock(); // Jalankan langsung
